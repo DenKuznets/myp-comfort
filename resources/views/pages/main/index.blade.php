289ae6fd289d35c8bab21ac5dc2e81106af3a2a8
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-04-15 13:53:55 +0300

    init commit

---
+++ resources/views/pages/main/index.blade.php
@@ -0,0 +1,10 @@
+@extends("master")
+@section('title', 'Техавто')
+@section("description", "Мы поможем вам подготовить документы для регистрации изменений в конструкцию вашего ТС,
+или переоборудование ТС в ГИБДД на законных основаниях.")
+@section("content")
+<main class="main">  
+  MAIN
+</main>
+
+@endsection
